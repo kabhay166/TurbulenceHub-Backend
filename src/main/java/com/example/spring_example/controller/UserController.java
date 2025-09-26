f1faf1437590c2bec30708ea47bbb5a8e47634af
Abhay Kumar <kabhay166@gmail.com>
2025-09-26 23:43:40 +0530

    removed unused imports from UserController

--- src/main/java/com/example/spring_example/controller/UserController.java
+++ src/main/java/com/example/spring_example/controller/UserController.java
@@ -14,14 +14,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.Optional;
